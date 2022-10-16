35d5604bee5559993c8c6fbeb741b8ac74e30848
rsPogromishe <rs.pogromishe@gmail.com>
2022-10-16 20:19:10 +0400

    Initial Commit

---
+++ HammerPizza/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HammerPizza
+//
+//  Created by Снытин Ростислав on 16.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
